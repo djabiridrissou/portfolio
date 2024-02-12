2dbf80d3edd3a9c0756b90a38ae5f255b6b7dc9e
IDRISSOU Djabir <81425788+djabiridrissou@users.noreply.github.com>
2024-02-12 09:33:41 +0000

    final version

---
+++ src/data/user.js
@@ -0,0 +1,66 @@
+const INFO = {
+	main: {
+		title: "Djabir's portfolio",
+		name: "Idriss",
+		email: "djabiridrissou@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/d11b1r",
+		github: "https://github.com/djabiridrissou",
+		linkedin: "https://www.linkedin.com/in/d11b1r-idrissou/",
+		instagram: "https://www.instagram.com/d11b1r/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/profile.php?id=100078183355301",
+	},
+
+	homepage: {
+		title: "Web and mobile application developer.",
+		description:
+			"I am a developer with expertise in Node.js, React.js and Next.js. I love learning new things and solving problems. I am willing to contribute to any technological advancement whatsoever. I am always looking for personal development and new challenges to take on. Since my passion became my job, I haven't slept anymore.",
+	},
+
+	about: {
+		title: "Idriss, I am. Locked up with a keyboard, I remake the world.",
+		description:
+			"",
+	},
+
+	articles: {
+		title: "Collection of notes and thoughts.",
+		description:
+			"I will often come back to add to this page with my personal notes and my own reflections, whether in the digital field or not. Likewise, I will make this possible for my acquaintances or anyone who would like to take up the pen.",
+	},
+
+	projects: [
+		{
+			title: "Ghana Audit Service AI Validator",
+			description:
+				"System designed to allow auditors on the ground to collect data and generate timely reports.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/typescript/typescript.png",
+			linkText: "",
+			link: "",
+		},
+
+		{
+			title: "Freights Analytics",
+			description:
+				"Programs dedicated to  Ghana Airports for the management of goods flows.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "",
+			link: "",
+		},
+
+		{
+			title: "Ukwazi",
+			description:
+				"System for predicting the result of an upcoming football match, based on matches already played.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/php/php.png",
+			linkText: "",
+			link: "",
+		},
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							Me, a laptop and a cup of coffee.
+						</div>
+
+						<div className="subtitle projects-subtitle">
+						I spent time working on projects, whether in TypeScript, Dart or PhP through Python and many others. 
+						I am continually working to establish myself as a senior developer. After computer studies, 
+						I quickly integrated a professional work environment, therefore most of my projects remain in the private domain. 
+						However, I am an active member of Developer for Diversity (Dev4Div) and West Africa Young Developers communities; 
+						which contribute to advances in the digital field across the African continent.
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
